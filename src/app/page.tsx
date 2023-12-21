cb54bbd8597d756f4d53859bc8b9487920cb2709
Andrew Megalaa <andrewmuhareb@gmail.com>
2023-12-21 18:19:26 -0500

    update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 import TranslationComponent from "./components/translation"; // Adjust the path accordingly
-import Info from "./components/info"; // Adjust the path accordingly
-import Header from "./components/header";
-import Cross from "../../public/cross.png";
-import Image from "next/image";
-import Footer from "./components/footer";
 
 const Page = () => {
   return <TranslationComponent />;
