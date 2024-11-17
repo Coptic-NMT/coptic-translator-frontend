46f12347f8ad147f8a4c095e21c8c14ce9ab580d
enismaxim1 <42425794+enismaxim1@users.noreply.github.com>
2024-11-16 21:34:15 -0500

    fix prod

---
+++ src/app/[locale]/components/translatorBanner.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useTranslations } from "next-intl";
+import { NextIntlClientProvider } from 'next-intl';
+import { useLocale } from "next-intl";
+import "./translation.css";
+
+const TranslatorBanner = ({ messages }: { messages: any }) => {
+  const locale = useLocale();
+  
+  return (
+    <NextIntlClientProvider messages={messages} locale={locale}>
+      <TranslatorContent />
+    </NextIntlClientProvider>
+  );
+};
+
+const TranslatorContent = () => {
+  const t = useTranslations("Page");
+  return (
+    <div className="w-full justify-center">
+      <div className="lg:flex-row lg:flex">
+        <div className="w-full lg:w-1/2">
+          <div className="mb-4">
+            <span className="text-scriptorium-grey">{t("try-other-translators")}</span>
+            <div className="flex flex-wrap gap-x-4 gap-y-2 mt-2">
+              <a
+                href="https://polytranslator.com/egyptian-arabic"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-scriptorium-red hover:underline"
+              >
+                Egyptian Arabic
+              </a>
+              <a
+                href="https://polytranslator.com/nubian"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-scriptorium-red hover:underline"
+              >
+                Nubian
+              </a>
+              <a
+                href="https://polytranslator.com/akkadian"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-scriptorium-red hover:underline"
+              >
+                Akkadian
+              </a>
+              <a
+                href="https://polytranslator.com/sumerian"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-scriptorium-red hover:underline"
+              >
+                Sumerian
+              </a>
+              <a
+                href="https://polytranslator.com/ancient-greek"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-scriptorium-red hover:underline"
+              >
+                Ancient Greek
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TranslatorBanner;
